3d280476dc6ff766be46e2fca3d205fb879f3538
Benjamin Pannell <benjamin@pannell.dev>
2020-06-13 12:08:23 +0100

    fix: Ensure that a user cannot remove themselves from a collection

--- src/api/role_assignments/remove_role_assignment.rs
+++ src/api/role_assignments/remove_role_assignment.rs
@@ -14,6 +14,10 @@ async fn remove_role_assignment_v3(
     let uid = parse_uuid!(token.oid, auth token oid);
     let tuid = parse_uuid!(info.user, user ID);
 
+    if tuid == uid {
+        return Err(APIError::new(400, "Bad Request", "You cannot remove yourself from a collection. Please request that another collection owner performs this for you."))
+    }
+
     let role = state.store.send(GetRoleAssignment { collection_id: cid, principal_id: uid }).await??;
     match role.role {
         Role::Owner => {
@@ -53,5 +57,25 @@ mod tests {
             collection_id: 1,
             principal_id: 2
         }).await.expect("the actor should have run").expect_err("The role assignment should not exist anymore");
+    }
+
+    #[actix_rt::test]
+    async fn remove_role_assignment_v3_self() {
+        test_log_init();
+
+        test_state!(state = [
+            StoreRoleAssignment {
+                collection_id: 1,
+                principal_id: 0,
+                role: Role::Owner,
+            }
+        ]);
+
+        test_request!(DELETE "/api/v3/collection/00000000000000000000000000000001/user/00000000000000000000000000000000" => BAD_REQUEST | state = state);
+
+        state.store.send(GetRoleAssignment {
+            collection_id: 1,
+            principal_id: 0
+        }).await.expect("the actor should have run").expect("The role assignment should still exist");
     }
 }

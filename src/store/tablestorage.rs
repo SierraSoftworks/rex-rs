2f2689dc92ea5d30836c95e42cac72224ea6049d
Benjamin Pannell <benjamin@pannell.dev>
2025-12-02 00:58:41 +0000

    fix: Resolve some potential issues with table storage

--- src/store/tablestorage.rs
+++ src/store/tablestorage.rs
@@ -11,7 +11,7 @@ use rand::seq::IteratorRandom;
 use serde::de::DeserializeOwned;
 use serde::Serialize;
 use std::{fmt::Debug, pin::Pin, sync::Arc};
-use tracing::Instrument;
+use tracing_batteries::prelude::*;
 
 type TableReference = Arc<TableClient>;
 
@@ -163,22 +163,20 @@ impl TableStorage {
     }
 
     #[instrument(err, skip(table, item), fields(otel.kind = "client", db.system = "TABLESTORAGE", db.operation = "PUT"))]
-    async fn store_single<ST, T, PK, RK>(
+    async fn store_single<ST, T>(
         table: TableReference,
         type_name: &str,
-        partition_key: PK,
-        row_key: RK,
+        partition_key: u128,
+        row_key: u128,
         item: ST,
     ) -> Result<T, APIError>
     where
         ST: Serialize + DeserializeOwned + Clone + Debug + Sync + Send,
-        T: From<ST>,
-        PK: AsRef<str> + Debug,
-        RK: AsRef<str> + Debug,
+        T: From<ST>
     {
         table
-            .partition_key_client(partition_key.as_ref())
-            .entity_client(row_key.as_ref())
+            .partition_key_client(format!("{partition_key:0>32x}"))
+            .entity_client(format!("{row_key:0>32x}"))
             .insert_or_replace(&item)?
             .into_future()
             .await
@@ -487,7 +485,7 @@ actor_handler!(GetRandomIdea|msg => Idea: get_random from ideas(TableStorageIdea
     filter = i -> tag_str.is_empty() || i.tags.split(',').any(|i| i == tag_str.as_str());
     not found = "We could not find any ideas in the collection you provided which matched your query. Please create some and try again.");
 
-actor_handler!(StoreIdea|msg => Idea: store_single in ideas(TableStorageIdea) where pk=format!("{:0>32x}", msg.collection), rk=format!("{:0>32x}", msg.id); return TableStorageIdea {
+actor_handler!(StoreIdea|msg => Idea: store_single in ideas(TableStorageIdea) where pk=msg.collection, rk=msg.id; return TableStorageIdea {
     collection_id: format!("{:0>32x}", msg.collection),
     id: format!("{:0>32x}", msg.id),
     name: msg.name.clone(),
@@ -505,7 +503,7 @@ actor_handler!(GetCollections|msg => Collection: get_all from collections(TableS
     context = [],
     filter = _i -> true);
 
-actor_handler!(StoreCollection|msg => Collection: store_single in collections(TableStorageCollection) where pk=format!("{:0>32x}", msg.principal_id), rk=format!("{:0>32x}", msg.collection_id); return TableStorageCollection {
+actor_handler!(StoreCollection|msg => Collection: store_single in collections(TableStorageCollection) where pk=msg.principal_id, rk=msg.collection_id; return TableStorageCollection {
     principal_id: format!("{:0>32x}", msg.principal_id),
     collection_id: format!("{:0>32x}", msg.collection_id),
     name: msg.name.clone(),
@@ -520,7 +518,7 @@ actor_handler!(GetRoleAssignments|msg => RoleAssignment: get_all from role_assig
     context = [],
     filter = _i -> true);
 
-actor_handler!(StoreRoleAssignment|msg => RoleAssignment: store_single in role_assignments(TableStorageRoleAssignment) where pk=format!("{:0>32x}", msg.collection_id), rk=format!("{:0>32x}", msg.principal_id); return TableStorageRoleAssignment {
+actor_handler!(StoreRoleAssignment|msg => RoleAssignment: store_single in role_assignments(TableStorageRoleAssignment) where pk=msg.collection_id, rk=msg.principal_id; return TableStorageRoleAssignment {
     collection_id: format!("{:0>32x}", msg.collection_id),
     principal_id: format!("{:0>32x}", msg.principal_id),
     role: msg.role.into(),
@@ -530,7 +528,7 @@ actor_handler!(RemoveRoleAssignment|msg: remove_single from role_assignments whe
 
 actor_handler!(GetUser|msg => models::User: get_single from users(TableStorageUser) where pk=msg.email_hash, rk=msg.email_hash; not found = "The user you are looking for could not be found. Please check that you have entered their email address correctly and try again.");
 
-actor_handler!(StoreUser|msg => models::User: store_single in users(TableStorageUser) where pk=format!("{:0>32x}", msg.email_hash), rk=format!("{:0>32x}", msg.email_hash); return TableStorageUser {
+actor_handler!(StoreUser|msg => models::User: store_single in users(TableStorageUser) where pk=msg.email_hash, rk=msg.email_hash; return TableStorageUser {
     email_hash: format!("{:0>32x}", msg.email_hash),
     row_key: format!("{:0>32x}", msg.email_hash),
     principal_id: format!("{:0>32x}", msg.principal_id),
